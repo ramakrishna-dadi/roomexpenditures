e18e30d4c898f14641634cf469739d73c0178353
ramakrishna-dadi <dramakrishna0509@gmail.com>
2015-08-16 13:37:49 +0530

    added config file
    added config file

--- assets/js/room.js
+++ assets/js/room.js
@@ -38,27 +38,35 @@ $(document).ready(function(){
                },
     submitHandler: function(form) {
                 $.ajax({
-                    type: "GET",
-                    url: "http://roomexpenditures-wpapp9.rhcloud.com/server/roomexp.php",
+                    type: "POST",
+                     dataType:'json',
+                    url: "server/roomexp.php",
                     data: $(form).serialize(),
                     timeout: 3000,
                     success: function(data) {
                         //alert(data);
                         
-                        if(data == 't') 
-                        {
+                        // if(data == 't') 
+                        // {
                            
-                            $('.exists').html('');
-                            $('.exists').append('<div class="alert alert-danger"><strong>Already </strong>exists. Try another name</div>');
-                            scroll(0,0);
-                        } 
-                        else if(data == "IS") 
+                        //     $('.exists').html('');
+                        //     $('.exists').append('<div class="alert alert-danger"><strong>Already </strong>exists. Try another name</div>');
+                        //     scroll(0,0);
+                        // } 
+                        // else if(data == "IS") 
+                        // {
+                        //     location.href = "q-and-a-management.php?msg=addtsuc";
+                        // }
+                        // else if(data == "f") 
+                        // {
+                        //     location.href = "q-and-a-management.php?msg=addtfail";
+                        // }
+                        if(data.success == true)
                         {
-                            location.href = "q-and-a-management.php?msg=addtsuc";
-                        }
-                        else if(data == "f") 
-                        {
-                            location.href = "q-and-a-management.php?msg=addtfail";
+                            //  location.href = '#page_home';
+                           // $.mobile.pageContainer.pagecontainer("change", "#page_home");
+                           window.location = window.location + '#page_home';
+                           window.location.reload();
                         }
                        
                     },
@@ -70,3 +78,32 @@ $(document).ready(function(){
           }
          });
 });
+ function load_expnd_list()
+ {
+    $.ajax({
+                    type: "POST",
+                    dataType:'json',
+                    url: "server/roomexp.php",
+                    data: {action:'load_expnd_list'},
+                    timeout: 3000,
+                    success: function(data) {
+                       $('#myTable tbody').html('');
+                       if(data.success == true)
+                       {
+                       for(i=0;i<= data.results.length;i++)
+                       {
+                            $('#myTable tbody').append('<tr><td>'+data.results[i]['desc']+'</td><td>'+data.results[i]['amount']+'</td><td>'+data.results[i]['pay_by']+'</td><td>'+data.results[i]['phani_amt']+'</td><td>'+data.results[i]['rk_amt']+'</td><td>'+data.results[i]['srinu_amt']+'</td><td>'+data.results[i]['created_on']+'</td></tr>');
+                       }
+                    }
+                    else
+                    {
+                        
+                        $('#myTable tbody').html('<tr><td colspan="7">No Expenditures Available<td></tr>');
+                    }
+                       
+                    },
+                    error: function() {
+                        
+                    }
+                });
+ }
